6542b0fe7877df0e1d5b2cddddcb335f884b7458
João Lucas Mazetti <jlmazetti03@hotmail.com>
2024-11-03 22:02:24 -0300

    Calculo de média de altura

--- Vetores/src/main/java/org/example/Main.java
+++ Vetores/src/main/java/org/example/Main.java
@@ -12,6 +12,18 @@ public class Main {
         int n = sc.nextInt();
         double[] vect = new double[n];
 
+        for (int i = 0; i < n; i++){
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0;
+        for (int i = 0; i < n; i++){
+            sum += vect[i];
+        }
+        double avg = sum / n;
+
+        System.out.printf("AVERAGE HEIGHT: %.2f%n", avg);
+
         sc.close();
     }
 }
